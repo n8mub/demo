4c60efefe8c24d5344d7c74edac92977f22a0ae4
n8mub <n8mub1@gmail.com>
2018-02-05 17:10:11 -0500

    aws lambda

---
+++ TestLambda/src/main/java/com/accenture/cwr37/TestLambda/LambdaRequestStreamHandler.java
@@ -0,0 +1,21 @@
+package com.accenture.cwr37.TestLambda;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.commons.io.IOUtils;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+
+public class LambdaRequestStreamHandler implements RequestStreamHandler {
+	public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) {
+		try {
+			String input = IOUtils.toString(inputStream, "UTF-8");
+			outputStream.write(("Hello World - " + input).getBytes());
+		} catch (IOException e) {
+			context.getLogger().log(e.getStackTrace().toString());
+		}
+	}
+}
---
+++ TestLambda/src/main/java/com/accenture/cwr37/dynamodb/PersonResponse.java
@@ -0,0 +1,16 @@
+package com.accenture.cwr37.dynamodb;
+
+public class PersonResponse {
+	private String message;
+	
+	// standard getters and setters
+	
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+    
+}
