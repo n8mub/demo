0b360de2547e11fcf1fe2d0a5e623277d8bdf690
n8mub <n8mub1@gmail.com>
2019-03-26 15:52:23 -0400

    added request mapping media types

--- Hospitals/src/main/java/com/tcs/hospitals/HospitalController.java
+++ Hospitals/src/main/java/com/tcs/hospitals/HospitalController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,12 +20,12 @@ public class HospitalController {
 	@Autowired
 	private HospitalService hospitalService;
 	
-	@RequestMapping(value="/hospitals",method=RequestMethod.GET)
+	@RequestMapping(value="/hospitals",produces=MediaType.APPLICATION_JSON_VALUE,method=RequestMethod.GET)
 	public @ResponseBody List<Hospital> getAllHospitals() {
 		return hospitalService.getAllHospitals();
 	}
 	
-	@RequestMapping(value="/hospitals/{id}",method=RequestMethod.GET)
+	@RequestMapping(value="/hospitals/{id}",produces=MediaType.APPLICATION_JSON_VALUE,method=RequestMethod.GET)
 	public @ResponseBody Hospital getHospital(@PathVariable("id") int id) {
 		return hospitalService.getHospital(id);
 	}
